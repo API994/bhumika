3ef4551782b3b1d8319271814800a2f11ff11bdf
API994 <78959793+API994@users.noreply.github.com>
2021-02-17 18:21:26 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,15 @@
+class example
+{
+public static void main(String args[])
+{
+int a=2,b=3;
+if(a>b)
+a=a-b;
+else
+b=b-a;
+System.out.println(a);
+System.out.println(b);
+System.out.println("I am Bhumika");
+System.out.println("I am Bhumikaaaa");
+}
+}
